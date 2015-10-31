42032cf86526779b4df8bfb7e12f4c4756df5c07
William Belle <william.belle@gmail.com>
2015-10-31 17:48:07 +0100

    convert data also to nodejs module

--- src/converter.js
+++ src/converter.js
@@ -9,7 +9,7 @@ var hash = {};
 var data = fs.readFileSync('data/postal-codes.tsv', 'utf8');
 data.split('\n').forEach(function (row) {
   var columns = row.split('\t');
-  if (columns.length === 3){
+  if (columns.length === 4){
     if (columns[0] in hash){
       hash[columns[0]] = hash[columns[0]] + ' - ' + columns[1];
     } else {
@@ -18,3 +18,4 @@ data.split('\n').forEach(function (row) {
   }
 });
 fs.writeFileSync('dist/postal-codes.json', JSON.stringify(hash), 'utf8');
+fs.writeFileSync('dist-node/index.js', 'module.exports=' + JSON.stringify(hash) + ';', 'utf8');
