defd2b670182f8860be74c3060b0e7a58913d090
William Belle <william.belle@gmail.com>
2015-11-02 17:14:33 +0100

    improve unit tests

--- test/test.js
+++ test/test.js
@@ -4,14 +4,19 @@
   var should = require("chai").should();
   var codes = require("../dist-node/index.js");
 
-  describe('swizerland-postal-code', function(){
+  describe('switzerland-postal-code', function(){
     it("should return a location", function(){
       var cologny = codes['1223'];
       cologny.should.equal('Cologny');
     });
 
-    it("should contains at least 350 items", function(){
-      var enoughLocation = Object.keys(codes).length > 350;
+    it("should return a composed location", function(){
+      var cologny = codes['1295'];
+      cologny.should.equal('Mies - Tannay');
+    });
+
+    it("should contains at least 650 items", function(){
+      var enoughLocation = Object.keys(codes).length > 650;
       enoughLocation.should.equal(true);
     });
   });
