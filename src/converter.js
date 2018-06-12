dc43d4fe9ae0ffd012e1558d90e2f18e80732593
William Belle <william.belle@gmail.com>
2018-06-12 20:23:26 +0200

    move to eslint

--- src/converter.js
+++ src/converter.js
@@ -2,20 +2,20 @@
 
 'use strict';
 
-var fs   = require('fs');
+var fs = require('fs');
 
 var codes = {};
-var full  = {};
+var full = {};
 var data = fs.readFileSync('data/postal-codes.tsv', 'utf8');
 data.split('\n').forEach(function (row) {
   var columns = row.split('\t');
-  if (columns.length === 5){
-    if (columns[0] in codes){
+  if (columns.length === 5) {
+    if (columns[0] in codes) {
       codes[columns[0]] = codes[columns[0]] + ' - ' + columns[1];
     } else {
       codes[columns[0]] = columns[1];
     }
-    if (columns[0] in full){
+    if (columns[0] in full) {
       var list = full[columns[0]];
       list.push(createObj(columns));
       full[columns[0]] = list;
@@ -26,17 +26,19 @@ data.split('\n').forEach(function (row) {
 });
 writeFile('dist/postal-codes.json', JSON.stringify(codes));
 writeFile('dist/postal-codes-full.json', JSON.stringify(full));
-writeFile('dist-node/index.js', 'module.exports=' + JSON.stringify(codes) + ';');
+writeFile(
+  'dist-node/index.js', 'module.exports=' + JSON.stringify(codes) + ';'
+);
 
-function createObj(columns){
+function createObj (columns) {
   return {
-    name:      columns[1],
-    canton:    columns[2],
-    latitude:  columns[3],
-    longitude: columns[4],
+    name: columns[1],
+    canton: columns[2],
+    latitude: columns[3],
+    longitude: columns[4]
   };
 }
 
-function writeFile(file, content){
+function writeFile (file, content) {
   fs.writeFileSync(file, content, 'utf8');
 }
--- test/test-postal-code.js
+++ test/test-postal-code.js
@@ -1,39 +1,37 @@
-(function(){
-  'use strict';
+'use strict';
 
-  var should    = require("chai").should();
-  var nodeCodes = require("../dist-node/index.js");
-  var jsonCodes = require("../dist/postal-codes.json");
+var should = require('chai').should();
+var nodeCodes = require('../dist-node/index.js');
+var jsonCodes = require('../dist/postal-codes.json');
 
-  var codes = [nodeCodes, jsonCodes];
+var codes = [nodeCodes, jsonCodes];
 
-  describe('switzerland-postal-code', function(){
-    it('should return a location', function(){
-      codes.forEach(function(item){
-        var cologny = item['1223'];
-        cologny.should.equal('Cologny');
-      });
-    });
-
-    it('should return a composed location', function(){
-      codes.forEach(function(item){
-        var miesTannay = item['1295'];
-        miesTannay.should.equal('Mies - Tannay');
-      });
-    });
-
-    it('should return undefined', function(){
-      codes.forEach(function(item){
-        var notDefined = item['10000'];
-        should.equal(notDefined, undefined);
-      });
-    });
-
-    it('should contains at least 3000 items', function(){
-      codes.forEach(function(item){
-        var enoughLocation = Object.keys(item).length > 3000;
-        enoughLocation.should.equal(true);
-      });
+describe('switzerland-postal-code', function () {
+  it('should return a location', function () {
+    codes.forEach(function (item) {
+      var cologny = item['1223'];
+      cologny.should.equal('Cologny');
     });
   });
-})();
+
+  it('should return a composed location', function () {
+    codes.forEach(function (item) {
+      var miesTannay = item['1295'];
+      miesTannay.should.equal('Mies - Tannay');
+    });
+  });
+
+  it('should return undefined', function () {
+    codes.forEach(function (item) {
+      var notDefined = item['10000'];
+      should.equal(notDefined, undefined);
+    });
+  });
+
+  it('should contains at least 3000 items', function () {
+    codes.forEach(function (item) {
+      var enoughLocation = Object.keys(item).length > 3000;
+      enoughLocation.should.equal(true);
+    });
+  });
+});
